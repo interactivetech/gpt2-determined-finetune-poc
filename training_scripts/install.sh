350bdbf3940061bfe948eecac227273c416f28eb
andrew <inter>
2023-06-07 19:32:10 +0200

    update

---
+++ training_scripts/install.sh
@@ -0,0 +1,6 @@
+pip install bitsandbytes
+pip install git+https://github.com/huggingface/transformers.git 
+pip install git+https://github.com/huggingface/peft.git
+pip install git+https://github.com/huggingface/accelerate.git
+pip install einops
+pip install datasets
