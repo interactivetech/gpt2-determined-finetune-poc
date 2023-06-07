31f52d8a31156519f1e7c4ad5cc5ce8f5757c400
andrew <inter>
2023-06-07 20:49:55 +0200

    update

--- training_scripts/install.sh
+++ training_scripts/install.sh
@@ -4,3 +4,4 @@ pip install git+https://github.com/huggingface/peft.git
 pip install git+https://github.com/huggingface/accelerate.git
 pip install einops
 pip install datasets
+pip install torch==1.13.0+cu116 torchvision==0.14.0+cu116 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu116
